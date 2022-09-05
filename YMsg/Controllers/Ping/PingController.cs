9a965888c8102b4ede4773af3ee73cdd6c9c9e27
demydko.yuri <yuridemydko@icloud.com>
2022-09-06 00:06:42 +0400

    Update PingController.cs

--- YMsg/Controllers/Ping/PingController.cs
+++ YMsg/Controllers/Ping/PingController.cs
@@ -13,8 +13,7 @@ public class PingController:ControllerBase
         return Ok(new
         {
             Message = "YMsg - Hello",
-            Today = DateTime.Now,
-            IP = Request.HttpContext.Connection?.RemoteIpAddress?.MapToIPv4().ToString()
+            Today = DateTime.Now
         });
     }
     
